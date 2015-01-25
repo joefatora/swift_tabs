c71da3b8a7c77db0e6b9056da513458075b93ce6
Joe Fatora <jfatora@gmail.com>
2015-01-25 18:25:03 -0500

    Initial Commit

---
+++ tabs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tabs
+//
+//  Created by Joseph Fatora on 1/25/15.
+//  Copyright (c) 2015 Joseph Fatora. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
